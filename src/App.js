d8cd7f13bb1c7d96e3d926630a8c6edcec3541ca
Jung Young Jun <dudwns0921@gmail.com>
2022-06-25 21:45:51 +0900

    :lipstick: Complete left section UI and overall style

--- src/App.js
+++ src/App.js
@@ -2,17 +2,46 @@ import React from 'react'
 import styled from 'styled-components'
 
 import triple2x from './public/images/triple2x.png'
+import playStore2x from './public/images/play-store2x.png'
+import badgeApple4x from './public/images/badge-apple4x.png'
+import TextBox from './components/TextBox'
 
 function App() {
   return (
     <Container>
       <RightSec>
-        <div>
-          <img src={triple2x} alt="main" />
+        <main>
+          <img src={triple2x} alt="triple" />
           <span>2019년 2월 기준</span>
-        </div>
+        </main>
       </RightSec>
-      <LeftSec>Left</LeftSec>
+      <LeftSec>
+        <main>
+          <section>
+            <TextBox amount="350만 명" category="사용자" />
+            <TextBox amount="21만 개" category="리뷰" />
+            <TextBox amount="650만 개" category="저장" />
+          </section>
+          <section>
+            <div>
+              <img src={playStore2x} alt="googlePlay" />
+              <span>
+                2018 구글 플레이스토어
+                <br />
+                올해의 앱 최우수상 수상
+              </span>
+            </div>
+            <div>
+              <img src={badgeApple4x} alt="apple" />
+              <span>
+                2018 애플 앱스토어
+                <br />
+                오늘의 여행앱 선정
+              </span>
+            </div>
+          </section>
+        </main>
+      </LeftSec>
     </Container>
   )
 }
@@ -21,29 +50,55 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   min-width: 1200px;
-  height: 50vh;
+  height: 552px;
+  padding-top: 150px;
+  font-family: var(--app-font-style);
+  color: var(--app-font-color);
 `
 const RightSec = styled.section`
   display: flex;
   justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  div {
+  main {
     position: relative;
-    width: 50%;
+    width: 400px;
+    height: 338px;
     display: flex;
     justify-content: center;
     img {
       width: 100%;
-      height: auto;
+      height: 100%;
     }
     span {
       position: absolute;
-      bottom: 0;
+      font-size: 15px;
+      bottom: 43px;
     }
   }
 `
-const LeftSec = styled.section``
+const LeftSec = styled.section`
+  display: flex;
+  flex-direction: column;
+  main {
+    display: grid;
+    section:nth-child(2) {
+      display: flex;
+      margin-top: 30px;
+      div {
+        display: flex;
+        align-items: center;
+        margin-right: 39px;
+        img {
+          width: 72px;
+          padding-right: 10px;
+        }
+        span {
+          font-weight: bold;
+          font-size: 14px;
+          line-height: 22px;
+        }
+      }
+    }
+  }
+`
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { reset } from 'styled-reset'
+import { createGlobalStyle } from 'styled-components'
 
 import App from './App.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  #root {
+    display: flex;
+    justify-content: center;
+  }
+  :root {
+    --app-font-style: sans-serif;
+    --app-font-color: rgba(58, 58, 58, 0.7); 
+  }
+  ${reset}
+`
 
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+)
