72e2a4f5d6595859118a7281412fcbaa42c0e4fe
Jung Young Jun <dudwns0921@gmail.com>
2022-06-26 14:24:37 +0900

    :dizzy: Add counter animation

--- src/components/Record.js
+++ src/components/Record.js
@@ -1,8 +1,21 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 function Record(props) {
   const [count, setCount] = useState(0)
+  useEffect(() => {
+    let delay = 1200 / props.amount
+    if (count < props.amount - 4) {
+      setTimeout(() => {
+        setCount((prev) => prev + 1)
+      }, delay)
+    } else if (props.amount - 4 <= count && count < props.amount) {
+      delay = 800 / 4
+      setTimeout(() => {
+        setCount((prev) => prev + 1)
+      }, delay)
+    }
+  }, [count, props.amount])
   return (
     <Container>
       <strong>
